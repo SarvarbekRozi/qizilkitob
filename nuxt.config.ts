a871d1bef8160ee66a6aea1bb50ab5ebc9743ec3
SarvarbekRozi <sarvarbekr1203@gmail.com>
2026-03-13 14:34:20 +0500

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -93,7 +93,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://admin-qizilkitob-uz/api/v2'
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://admin.qizilkitob.uz/api/v2'
     }
   },
 
